c8080408fa1c4991f3d345a406afd842e70f557f
woojin <woojin.seo@icloud.com>
2021-06-12 16:12:53 -0500

    Multiple random face generator

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
 import "./App.css";
-import { Mouth } from "./Mouth";
-import { Eyes } from "./Eyes";
-import { BackgroundCircle } from "./BackgroundCircle";
-const width = 960;
-const height = 500;
-const centerY = height / 2;
-const centerX = width / 2;
-const strokeWidth = 20;
-const eyeOffsetX = 90;
-const eyeOffsetY = 100;
-const eyeRadius = 40;
-const mouthWidth = 20;
-const mouthRadius = 130;
+import { range } from "d3";
+import { Face } from "./Face";
+const width = 160;
+const height = 160;
 
-const App = () => {
-  return (
-    <div
-      style={{
-        margin: 0,
-        overflow: "hidden",
-      }}
-    >
-      <svg width={width} height={height}>
-        <g transform={`translate(${centerX},${centerY})`}>
-          <BackgroundCircle
-            radius={centerY - strokeWidth / 2}
-            strokeWidth={strokeWidth}
-          />
-          <Eyes
-            eyeOffsetX={eyeOffsetX}
-            eyeOffsetY={eyeOffsetY}
-            eyeRadius={eyeRadius}
-          />
-          <Mouth mouthRadius={mouthRadius} mouthWidth={mouthWidth} />
-        </g>
-      </svg>
-    </div>
-  );
-};
+const array = range(5 * 3);
+
+const App = () =>
+  array.map(() => {
+    return (
+      <Face
+        width={width}
+        height={height}
+        centerY={height / 2}
+        centerX={width / 2}
+        strokeWidth={10 + Math.random() * 7}
+        eyeOffsetX={30 + Math.random() * 5}
+        eyeOffsetY={30 + Math.random() * 7}
+        eyeRadius={10 + Math.random() * 5}
+        mouthWidth={1 + Math.random() * 6}
+        mouthRadius={40 + Math.random() * 4}
+      />
+    );
+  });
 
 export default App;
---
+++ src/Face.js
@@ -0,0 +1,34 @@
+import { FaceContainer } from "./FaceContainer";
+import { BackgroundCircle } from "./BackgroundCircle";
+import { Eyes } from "./Eyes";
+import { Mouth } from "./Mouth";
+export const Face = ({
+  width,
+  height,
+  centerX,
+  centerY,
+  strokeWidth,
+  eyeOffsetX,
+  eyeOffsetY,
+  eyeRadius,
+  mouthRadius,
+  mouthWidth,
+}) => (
+  <FaceContainer
+    width={width}
+    height={height}
+    centerX={centerX}
+    centerY={centerY}
+  >
+    <BackgroundCircle
+      radius={centerY - strokeWidth / 2}
+      strokeWidth={strokeWidth}
+    />
+    <Eyes
+      eyeOffsetX={eyeOffsetX}
+      eyeOffsetY={eyeOffsetY}
+      eyeRadius={eyeRadius}
+    />
+    <Mouth mouthRadius={mouthRadius} mouthWidth={mouthWidth} />
+  </FaceContainer>
+);
