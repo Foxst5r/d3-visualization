644ea7f028ad900251fcc3c27de9ec4c35a409c9
woojin <woojin.seo@icloud.com>
2021-06-11 19:50:28 -0500

    Smile finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { arc } from "d3";
 
-function App() {
+const width = 960;
+const height = 500;
+const centerY = height / 2;
+const centerX = width / 2;
+const strokeWidth = 20;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 130;
+
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: 0,
+        overflow: "hidden",
+      }}
+    >
+      <svg width={width} height={height}>
+        <g transform={`translate(${centerX},${centerY})`}>
+          <circle
+            fill="yellow"
+            stroke="black"
+            r={centerY - strokeWidth / 2}
+            stroke-width={strokeWidth}
+          ></circle>
+          <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius}></circle>
+          <circle cx={+eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius}></circle>
+          <path d={mouthArc()} />
+        </g>
+      </svg>
     </div>
   );
-}
+};
 
 export default App;
