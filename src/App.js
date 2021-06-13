f2dc02e704707ee14a5f69dabae10a84d0da4f52
woojin <woojin.seo@icloud.com>
2021-06-13 16:43:19 -0500

    circleTracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import React, { useState, useCallback } from "react";
 function App() {
-  const fetchText = async (url) => {
-    const response = await fetch(url);
-    return await response.text();
-  };
-  const csvUrl =
-    "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv";
+  const width = 960;
+  const height = 500;
+  const initialMousePosition = { x: width / 2, y: height / 2 };
+  const [mousePosition, setMousePosition] = useState(initialMousePosition);
+  const circleRadius = 30;
 
-  fetchText(csvUrl).then((text) => {
-    console.log(text);
-  });
-  //   fetch(url).then((response) => {
-  //   response.text().then((text) => {
-  //     console.log(text);
-  //   });
-  // });
-  return <div className="App"></div>;
+  const handleMouseMove = useCallback(
+    (event) => {
+      const { clientX, clientY } = event;
+      setMousePosition({ x: clientX, y: clientY });
+    },
+    [setMousePosition]
+  );
+
+  return (
+    <svg width={width} height={height} onMouseMove={handleMouseMove}>
+      <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} />
+    </svg>
+  );
 }
 
 export default App;
