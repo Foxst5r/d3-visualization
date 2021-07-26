a42b8a654889d911f543004aaf33ffd7c1ce2352
woojin <woojin.seo@icloud.com>
2021-07-26 16:16:02 -0500

    Brushing feature added to missing-migrant-map

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { DateHistorgram } from "./DateHistogram/index";
 import { useWorldAtlas } from "./useWorldAtlas";
 import { useData } from "./useData";
 import { BubbleMap } from "./BubbleMap/index";
+import { useState } from "react";
 const width = 1000;
 const height = 500;
 const dateHistogramSize = 0.2;
 
+const xValue = (d) => d["Reported Date"];
+
 function App() {
   const worldAtlas = useWorldAtlas();
   const data = useData();
+  const [brushExtent, setBrushExtent] = useState();
 
   if (!worldAtlas || !data) {
     return <pre>Loading... </pre>;
@@ -17,14 +21,22 @@ function App() {
 
   //r = sqrt(Area/pi)
 
+  const filteredData = data.filter((d) => {
+    const date = xValue(d);
+    if (brushExtent) return date > brushExtent[0] && date < brushExtent[1];
+    else return true;
+  });
+
   return (
     <svg width={width} height={height}>
-      <BubbleMap worldAtlas={worldAtlas} data={data} />
+      <BubbleMap worldAtlas={worldAtlas} data={filteredData} />
       <g transform={`translate(0,${height - dateHistogramSize * height})`}>
         <DateHistorgram
           data={data}
           height={dateHistogramSize * height}
           width={width}
+          setBrushExtent={setBrushExtent}
+          xValue={xValue}
         />
       </g>
     </svg>
--- src/DateHistogram/index.js
+++ src/DateHistogram/index.js
@@ -8,13 +8,24 @@ import {
   bin,
   timeMonths,
   sum,
+  brushX,
+  select,
 } from "d3";
 import { AxisBottom } from "./AxisBottom";
 import { AxisLeft } from "./AxisLeft";
+import { useRef, useEffect } from "react";
 
-export const DateHistorgram = ({ data, width, height }) => {
+export const DateHistorgram = ({
+  data,
+  width,
+  height,
+  setBrushExtent,
+  xValue,
+}) => {
   const margin = { top: 0, right: 30, bottom: 20, left: 45 };
-  const xValue = (d) => d["Reported Date"];
+
+  const brushRef = useRef();
+
   const xAxisLabel = "Reported Date";
 
   const xAxisLabelOffset = 80;
@@ -51,6 +62,18 @@ export const DateHistorgram = ({ data, width, height }) => {
     .domain([0, max(binnedData, (d) => d.y)])
     .range([innerHeight, 0])
     .nice();
+
+  useEffect(() => {
+    const brush = brushX().extent([
+      [0, 0],
+      [innerWidth, innerHeight],
+    ]);
+    brush(select(brushRef.current));
+    brush.on("brush end", (event) => {
+      setBrushExtent(event.selection && event.selection.map(xScale.invert));
+    });
+  }, [innerHeight, innerWidth]);
+
   return (
     <>
       <rect width={width} height={height} fill="white" />
@@ -90,6 +113,7 @@ export const DateHistorgram = ({ data, width, height }) => {
           tooltipFormat={(d) => d}
           innerHeight={innerHeight}
         ></Marks>
+        <g ref={brushRef} />
       </g>
     </>
   );
